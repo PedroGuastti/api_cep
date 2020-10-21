fd0e70fc1e16ee8e9315fdb4e68a41b75549028f
Pedro Martimiano <pedro.guastti260@gmail.com>
2020-10-20 22:41:58 -0300

    Updates

---
+++ precache-manifest.5161689dc2ee88da79f044c7114086ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec904c101c33cf0618e10a43fe37110",
+    "url": "/api_cep/index.html"
+  },
+  {
+    "revision": "d46862c0dd2dc881baa2",
+    "url": "/api_cep/static/css/main.37396a49.chunk.css"
+  },
+  {
+    "revision": "a2c39b44f3c19dec3bcf",
+    "url": "/api_cep/static/js/2.afcf5011.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api_cep/static/js/2.afcf5011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46862c0dd2dc881baa2",
+    "url": "/api_cep/static/js/main.72cccb8b.chunk.js"
+  },
+  {
+    "revision": "6d9dc33e090d344d4436",
+    "url": "/api_cep/static/js/runtime-main.59042d61.js"
+  }
+]);
